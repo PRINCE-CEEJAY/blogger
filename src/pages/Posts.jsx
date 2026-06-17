eb5e80e5e72b61bb1e3a0feebf0ef5df050f59aa
PRINCE-CEEJAY <ceejaydroidprince@gmail.com>
2026-06-17 01:25:29 -0400

    personalizing user contents

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,13 @@
+import { useAuth } from '@/context/AuthContext';
+
 export default function Posts() {
-    return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-black/30'>
-            <h1 className='text-4xl font-bold'>Posts Page</h1>
-        </div>
-    )
+  const { user } = useAuth();
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-black/30'>
+      <h1 className='text-4xl font-bold'>Posts Page</h1>
+      <h1 className='mt-12 font-bold text-3xl text-blue-800'>
+        {user && `Welcome dear ${user.name.toUpperCase()}`}
+      </h1>
+    </div>
+  );
 }
-
-
